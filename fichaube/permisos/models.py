2f73dedce3e593350ef01ccc2dfb0c7c455fbf61
Macarena Gárate M <31232682+Macagarate@users.noreply.github.com>
2019-10-17 16:07:12 -0300

    Implementacion Modelos
    Ficha, Alumno, Permisos, Usuario, Area, Especialidad, Registros

--- fichaube/permisos/models.py
+++ fichaube/permisos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from usuarios.models import Usuario
+from fichas.models import Ficha
 
 # Create your models here.
+
+########### CLASE PERMISOS #########################
+
+class Permiso(models.Model):
+  usuario =  models.ForeignKey(Usuario, on_delete = models.CASCADE)
+  ficha = models.ForeignKey(Ficha, on_delete = models.CASCADE)
--- fichaube/usuarios/models.py
+++ fichaube/usuarios/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date, datetime
 
 # Create your models here.
+
+########### CLASE USUARIO #########################
+
+class Usuario(models.Model):
+
+  nombre = models.CharField(max_length = 50, null=False)
+  apellidos = models.CharField(max_length = 50, null=False)
+  rut = models.CharField(max_length = 40, unique=True, null=False)
+  coordinador = models.BooleanField(default=False, null=False)
+  profesional = models.BooleanField(default=False, null=False)
+  administrativo = models.BooleanField(default=False, null=False)
+  mantenedor = models.BooleanField(default=False, null=False)
+  asistente_social = models.BooleanField(default=False, null=False)
+
+  user = models.OneToOneField(User, on_delete = models.SET_NULL) #USUARIO
+
+
+  def __str__(self):
+    return 'Rut={0}, Nombre={1}, Apellido={2}, Rut{3}, Coordinador{4}, Profesional{5}, Administrador{6}, Mantenedor{7}, Asistente{8}'.format(self.rut, self.nombre, self.apellidos, self.rut, self.coordinador, self.profesional, self.administrativo, self.mantenedor, self.asistente_social)
