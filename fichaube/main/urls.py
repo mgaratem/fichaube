513bba28802507158ed6e11a60f6d00979e19671
panchoso2 <fran_rhino@hotmail.com>
2019-12-10 20:09:37 -0300

    Areas en modelo html que corresponde
    El crud de Areas ahora posee el HTML base del sitio

--- fichaube/main/urls.py
+++ fichaube/main/urls.py
@@ -21,19 +21,22 @@ urlpatterns = [
     path('cambiar_pass/', views.cambiar_pass, name="cambiar_pass"),
 
 
+
+
+    ######## TENGO QUE MOVER TODAS ESTAS URL'S XDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
     # html's, estos despues se eliminaran*************
-    path('agregar-Area', areas.views.testpancho01, name="agregar-Area"),
-    path('editar-Area/<int:idArea>', areas.views.testpancho02, name='editar-Area'),
+    path('agregar_Area', areas.views.testpancho01, name="agregar_Area"),
+    path('editar_Area/<int:idArea>', areas.views.testpancho02, name='editar_Area'),
     path('agregar-Especialidad', areas.views.testpancho03, name="agregar-Especialidad"),
     path('editar-Especialidad/<int:idEspecialidad>/<int:idArea>', areas.views.testpancho04, name="editar-Especialidad"),
 
     # controladores de Areas
     path('agregarArea', areas.views.agregarArea, name="agregarArea"),
     path('ajax/validarArea', areas.views.validarArea, name="validarArea"),
-    path('lista-Areas', areas.views.listarAreas, name="lista-Areas"),
+    path('lista_Areas', areas.views.listarAreas, name="lista_Areas"),
     path('eliminarArea/<int:idArea>', areas.views.eliminarArea, name="eliminarArea"),
     path('editarArea', areas.views.editarArea, name="editarArea"),
-    path('editar-Area/ajax/validarArea', areas.views.validarArea, name="validarArea"), # esta URL deberia eliminarla, y aprovechar la de mas arriba usando regex !!!
+    path('editar_Area/ajax/validarArea', areas.views.validarArea, name="validarArea"), # esta URL deberia eliminarla, y aprovechar la de mas arriba usando regex !!!
 
     # controladores de Especialidades
     path('lista-Especialidades/<int:idArea>', areas.views.listarEspecialidades, name="lista-Especialidades"),
