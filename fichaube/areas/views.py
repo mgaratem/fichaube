2d4974782959d802de90f8cb20a63a7c8ab77ceb
panchoso2 <fran_rhino@hotmail.com>
2019-12-19 19:36:43 -0300

    login requerido para entrar al modulo de areas y especialidades
    no se puede acceder al modulo de areas y especialidades sin iniciar sesion

--- fichaube/areas/views.py
+++ fichaube/areas/views.py
@@ -1,3 +1,12 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User, Group
+from datetime import date, datetime
+import logging
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
@@ -13,16 +22,19 @@ from django.template import *
 
 
 # html para agregar area
+@login_required()
 def testpancho01(request):
     return render(request, 'agregar_Area.html',{})
 
 # html para editar area
+@login_required()
 def testpancho02(request, idArea):
     area = Area.objects.get(pk = idArea)
     nombreArea = area.nombreArea
     return render(request, 'editar_Area.html', {'idArea': idArea, 'nombreArea': nombreArea})
 
 # html para agregar especialidad
+@login_required()
 def testpancho03(request):
     if request.method == 'POST':
         idArea = request.POST.get("idArea")
@@ -31,6 +43,7 @@ def testpancho03(request):
         return render(request, 'agregar_Especialidad.html', {'idArea': idArea, 'nombreArea': nombreArea})
 
 # html para editar una especialidad
+@login_required()
 def testpancho04(request, idEspecialidad, idArea):
     especialidad = Especialidad.objects.get(pk = idEspecialidad)
     nombreEspecialidad = especialidad.nombreEspecialidad
@@ -40,6 +53,7 @@ def testpancho04(request, idEspecialidad, idArea):
 
 
 # funcion para agregar areas
+@login_required()
 def agregarArea(request):
     if request.method == 'POST':
         area = request.POST.get("area")
@@ -49,6 +63,7 @@ def agregarArea(request):
         return redirect('areas:lista_Areas')
 
 # funcion para corroborar si el area ya existe, se utiliza via AJAX
+@login_required()
 def validarArea(request):
     area = request.GET.get('area', None)
     data = {
@@ -57,18 +72,21 @@ def validarArea(request):
     return JsonResponse(data)
 
 # funcion y html para listar Areas
+@login_required()
 def listarAreas(request):
     listaAreas = Area.objects.all()
     return render(request, "lista_Areas.html", {"areas": listaAreas})
 
 # funcion para eliminar una Area dado un ID
+@login_required()
 def eliminarArea(request, idArea):
     # idArea = request.GET.get("idArea")
     Area.objects.filter(id=idArea).delete()
     return HttpResponseRedirect(reverse("areas:lista_Areas"))
-#******************FALTA eliminar en cascada
+
 
 # funcion para editar el nombre de un area
+@login_required()
 def editarArea(request):
     if request.method == 'POST':
         idArea = request.POST.get("idArea")
@@ -81,11 +99,13 @@ def editarArea(request):
 
 
 # funcion para listar todas las especialidades
+@login_required()
 def listarEspecialidades(request, idArea):
     listaEspecialides = Especialidad.objects.filter(area_id = idArea)
     return render(request, "lista_Especialidades.html", {"especialidades": listaEspecialides, "idArea": idArea})
 
 # funcion para agregar una nueva especialidad
+@login_required()
 def agregarEspecialidad(request):
     if request.method == 'POST':
         idArea = request.POST.get("idArea")
@@ -100,7 +120,7 @@ def agregarEspecialidad(request):
     return True
 
 # funcion para corroborar si la especialidad ya existe, se utiliza via AJAX
-# ****************************** Esto deberia buscar TODAS las especialidades, o solo las de ESA Area ????
+@login_required()
 def validarEspecialidad(request):
     especialidad = request.GET.get('especialidad', None)
     data = {
@@ -109,12 +129,14 @@ def validarEspecialidad(request):
     return JsonResponse(data)
 
 # funcion para eliminar una Especialidad dado un ID
+@login_required()
 def eliminarEspecialidad(request, idEspecialidad, idArea):
     Especialidad.objects.filter(id=idEspecialidad).delete()
     direccion = "/areas/lista_Especialidades/" + str(idArea)
     return redirect(direccion)
 
 # funcion para editar el nombre de una especialidad
+@login_required()
 def editarEspecialidad(request):
     if request.method == 'POST':
         idArea = request.POST.get("idArea")
