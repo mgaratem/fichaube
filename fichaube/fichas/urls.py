44460f25d031da93692f3c5d0adbbda67721df33
panchoso2 <fran_rhino@hotmail.com>
2019-12-20 23:26:10 -0300

    Generacion de reportes
    - Se genera el reporte en formato xlsx
    - Soluciones a otros detalles menores

--- fichaube/fichas/urls.py
+++ fichaube/fichas/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from . import views
 from .views import GeneratePdf
+from .views import ReporteExcel
 
 app_name = 'fichas'
 
@@ -10,5 +11,6 @@ urlpatterns = [
     path('confirmacion_ficha/<int:id_alumno>', views.confirmarCreacionFicha, name="confirmarCreacionFicha"),
     path('crear_registro/<int:id_alumno>', views.crearRegistro, name="crearRegistro"),
     path('reportes/', views.reportes, name="reportes"),
+    path('generarReporte', ReporteExcel.as_view(), name="generarReporte"),
     path('crear_pdf_ficha/<int:id_alumno>', GeneratePdf.as_view(), name="crearFichaPDF"),
 ]
